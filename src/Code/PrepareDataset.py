edf4ea192010b525e3a9554b22d8c51bedd169ac
Marc-Antoine Nadeau <nadeau.marc-antoine@hotmail.com>
2024-11-02 16:13:08 -0400

    Part 2

--- src/Code/PrepareDataset.py
+++ src/Code/PrepareDataset.py
@@ -1,1 +1,11 @@
 
+from medmnist import OrganAMNIST, INFO
+import ssl
+import matplotlib.pyplot as plt
+
+# Bypass SSL verification for dataset download
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Load the OrganAMNIST dataset information and download the train dataset
+train_dataset = OrganAMNIST(split="train", download=True)
+
